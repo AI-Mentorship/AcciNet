196883869e62f8d97c27b96ed743b0c0e08a4025
MahdMalik <mahdmahdmalik@gmail.com>
2025-10-05 22:36:13 -0500

    updated file format to be more correct. Now im gonna make my own branch

---
+++ .history/models/gradient_boosting/analysis_20251005215411.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score,accuracy_score
+from sklearn.utils.class_weight import compute_sample_weight
+from imblearn.under_sampling import RandomUnderSampler
+import pickle
+
+with open("gradient_boosting_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+    (_, _, X_test, y_test) = get
+    # 5. Make predictions
+    y_pred = model.predict(X_test)
+    y_proba = model.predict_proba(X_test)[:, 1]  # probability for positive class
+
+    # 6. Evaluatef
+    print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+    print("\nClassification Report:\n", classification_report(y_test, y_pred))
+    print("ROC-AUC Score:", roc_auc_score(y_test, y_proba))
+
+    print("Accuracy:", accuracy_score(y_test, y_pred))
