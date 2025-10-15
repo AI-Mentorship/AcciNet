4812b718bc5a26614229eb9de5736f21a93fcb63
Rodolfo Gonzalez <rudynoot123@gmail.com>
2025-10-15 12:42:05 -0500

    Cleaned geo-json for state segmentation limited to Texas & its respective counties

--- app/backend/clean-geo-json.py
+++ app/backend/clean-geo-json.py
@@ -1,18 +1,32 @@
 import geopandas as gpd 
 import json
+import chardet 
+import os
 
-with open("/home/rjg/coding/aim/accinet/frontend/accinet/public/geo-json/us-state.json", 
-          "r", encoding="utf-8", errors="replace") as f:
-    data = json.load(f)
+#ascii UTF-8 encoded
+geo_files = '/home/rjg/coding/aim/accinet/app/frontend/accinet/public/geo-json/'
+us_geo = geo_files +"us-state.json"
+counties_geo = geo_files + "us-counties-utf8.json"
 
-print(data)
+gdf_states = gpd.read_file(us_geo)
+#print(gdf_states)
 
-'''
-gdf_state = gpd.read_file("/home/rjg/coding/aim/accinet/frontend/accinet/public/geo-json/us-state.json")
-gdf_county = gpd.read_file("/home/rjg/coding/aim/accinet/frontend/accinet/public/geo-json/us-counties.json")
+gdf_counties = gpd.read_file(counties_geo)
+#print(gdf_texas)
 
-texas_code = 48
 
-texas_coord = gdf[gdf["NAME"]=="Texas"]
-print(texas_coord)
-'''
+
+col = 'STATE'
+state_code = '48'
+texas_mask = gdf_counties[col] == state_code
+gdf_texas = gdf_counties[texas_mask]
+
+texas_state_mask = gdf_states[col] == state_code
+gdf_state_texas = gdf_states[texas_state_mask]
+print(gdf_state_texas)
+
+#print(gdf_texas)
+
+#gdf_texas.to_file('texas-counties.json',driver='GeoJSON')
+gdf_state_texas.to_file( geo_files+ 'texas-state.json',driver='GeoJSON')
+
