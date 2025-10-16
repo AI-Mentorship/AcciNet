0559e32a59f773d263501111a304cdfb31fe72ac
MahdMalik <mahdmahdmalik@gmail.com>
2025-10-15 19:50:35 -0500

    applied a bandaid

--- models/gradient_boosting/getSets.py
+++ models/gradient_boosting/getSets.py
@@ -4,6 +4,13 @@ from sklearn.model_selection import train_test_split
 # 1. Load dataset
 df = pd.read_csv("data/final/true_preprocessed_data.csv")
 
+# df0 = df[df["label"] == 0]
+# df1 = df[df["label"] == 1]
+
+# df0 = df0.sample(df1.shape[0])
+
+# df = pd.concat([df0, df1], ignore_index=True)
+
 X = df.drop(columns="label")
 y = df["label"]
 
@@ -16,3 +23,6 @@ def getTrainData():
 
 def getTestData():
     return (X_test, y_test)
+
+def getListFeatures():
+    return X_train.columns
