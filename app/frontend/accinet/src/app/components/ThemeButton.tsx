316e9ed9976bcdb5aaef3c8f15b4198b919dff93
Rodolfo Gonzalez <rudynoot123@gmail.com>
2025-10-18 16:52:04 -0500

    Add google search component(visual only) and location based map viewing

--- app/frontend/accinet/src/app/components/ThemeButton.tsx
+++ app/frontend/accinet/src/app/components/ThemeButton.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import { useState } from 'react'
 import useTheme from '../hooks/useTheme'
@@ -5,11 +6,12 @@ import { MoonStar, Sun} from 'lucide-react'
 
 const ThemeButton = () => {
     const {isDark, toggle} = useTheme();
+    const theme= "w-10 h-10 p-2 rounded-md"
     return (
     <div>
         <button onClick={toggle}>
             {isDark==true?
-            <Sun className="w-5 h-5 bg-white text-shadow-yellow-500"/>:<MoonStar className="w-5 h-5 bg-black text-shadow-white"/>}
+            <Sun className={`${theme} bg-white text-yellow-500`}/>:<MoonStar className={`${theme} bg-black text-white`}/>}
         </button>
       </div>
   )
